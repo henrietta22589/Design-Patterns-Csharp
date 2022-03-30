5be0a4619c014abed38fdc576d2ffb2c7aa55bd0
henrietta22589 <93531231+henrietta22589@users.noreply.github.com>
2022-03-30 23:12:54 +0300

    Add files via upload

---
+++ C# design Patterns/Assignment3/EshopContext/Eshop.cs
@@ -0,0 +1,57 @@
+﻿using Assignment3.Models;
+using Assignment3.PaymentMethodStrategy;
+using Assignment3.VariationStrategy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.EshopContext
+{
+    internal class Eshop
+    {
+        private IEnumerable<IVariationStrategy> variations;
+        private IPaymentMethodStrategy paymentMethod;
+
+        public void SetPaymentMethod(IPaymentMethodStrategy method)
+        {
+
+            paymentMethod = method;
+        }
+
+
+        public void SetVariation(IEnumerable<IVariationStrategy> variationStrategies)
+        {
+
+            variations=variationStrategies;
+        }
+        public void CalculateCost(Tshirt tshirt)
+        {
+            foreach (var variation in variations)
+            {
+                variation.Cost(tshirt);
+            }
+            Console.WriteLine($"T-Shirt final price is {tshirt.Price} euros");
+        }
+        public void PayTshirt(decimal price)
+        {
+           
+
+            if (paymentMethod.Pay(price))
+            {
+                Console.WriteLine("Transaction was succesful");
+            }
+            else
+            {
+                Console.WriteLine("Transaction aborded");
+            }
+
+        }
+
+        public void Discount(Tshirt tshirt,decimal?perecntage)
+        {
+            paymentMethod.Discount(tshirt,perecntage);
+        }
+    }
+}
---
+++ C# design Patterns/Assignment3/Models/Tshirt.cs
@@ -0,0 +1,25 @@
+﻿using Assignment3.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public  class Tshirt
+    {
+        public Color Color;
+        public Size Size;
+        public Fabric Fabric ;
+        public decimal Price { get; set; }
+        public Tshirt(Color color, Fabric fabric ,Size size)
+        {
+            Color = color;
+            Fabric = fabric;
+            Size = size;
+        }
+
+        
+    }
+}
